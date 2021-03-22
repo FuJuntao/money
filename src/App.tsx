b5056755ded950fe52b5e1117635e48b8798d920
FuJuntao <fu.juntao358@gmail.com>
2021-03-22 19:27:45 +0800

    feat: add chakra ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,57 @@
-import React, { useEffect } from 'react';
+import {
+  ChakraProvider,
+  Container,
+  Heading,
+  Heading,
+  Spinner,
+  Spinner,
+  Stack,
+} from '@chakra-ui/react';
+import type { ExecutionResult } from 'graphql';
+import React, { useEffect, useState } from 'react';
 import { makeQuery } from './graphql-schemas/makeQuery';
 
-function App() {
+const useQuery = (source: string) => {
+  const [loading, setLoading] = useState(true);
+  const [result, setResult] = useState<null | ExecutionResult>(null);
+
   useEffect(() => {
     async function query() {
-      const result = await makeQuery(
-        '{ accounts { id name } transactions { id from to amount } }',
-      );
-      console.log('file: App.tsx ~ line 28 ~ query ~ result', result);
+      try {
+        const result = await makeQuery(source);
+        setResult(result);
+      } finally {
+        setLoading(false);
+      }
     }
 
     query();
   }, []);
 
+  return {
+    loading,
+    data: result?.data,
+    errors: result?.errors,
+  };
+};
+
+function App() {
+  const { data, loading } = useQuery('{ accounts { id name } }');
+
   return (
-    <div>
-      <h1>hello</h1>
-    </div>
+    <ChakraProvider>
+      <Heading as="h1">Hello</Heading>
+      <Heading>Accounts</Heading>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <Stack>
+          {data?.accounts.map((account: any) => (
+            <Heading as="h3">{account.name}</Heading>
+          ))}
+        </Stack>
+      )}
+    </ChakraProvider>
   );
 }
 
