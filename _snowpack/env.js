cb674557bb729cbbc47b612db4c47da88fb65845
FuJuntao <FuJuntao@users.noreply.github.com>
2021-05-24 16:11:42 +0000

    deploy: c58f1733ed42f19a1fa03720e18265f41edbd47b

--- _snowpack/env.js
+++ _snowpack/env.js
@@ -1,4 +1,4 @@
-export const SNOWPACK_PUBLIC_BASE_URL = "/${GITHUB_REPOSITORY#*/}";
+export const SNOWPACK_PUBLIC_BASE_URL = "/money";
 export const MODE = "production";
 export const NODE_ENV = "production";
 export const SSR = false;
