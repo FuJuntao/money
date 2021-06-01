0271bbee6f9ee7a7b27077c0a4442c86f5cb1166
FuJuntao <FuJuntao@users.noreply.github.com>
2021-06-01 07:14:57 +0000

    deploy: f0f4d7d6cb508eed2805ca7ff4a85598bdf93d29

--- dist/yup-config.js
+++ dist/yup-config.js
@@ -1,6 +1,7 @@
+import {Decimal} from "../_snowpack/pkg/decimaljs.js";
 import * as yup from "../_snowpack/pkg/yup.js";
 function maxDigitsAfterDecimal(maxDigits, message) {
-  return this.test("maxDigitsAfterDecimal", message ?? `\${path} must have ${maxDigits} digits or less after decimal`, (value = 0) => Number.isInteger(value * 10 ** maxDigits));
+  return this.test("maxDigitsAfterDecimal", message ?? `\${path} must have ${maxDigits} digits or less after decimal`, (value = 0) => new Decimal(value).dp() <= maxDigits);
 }
 yup.addMethod(yup.number, "maxDigits", maxDigitsAfterDecimal);
-//# sourceMappingURL=data:application/json;base64,ewogICJ2ZXJzaW9uIjogMywKICAic291cmNlcyI6IFsiL2hvbWUvcnVubmVyL3dvcmsvbW9uZXkvbW9uZXkvc3JjL3l1cC1jb25maWcudHMiXSwKICAibWFwcGluZ3MiOiAiQUFBQTtBQUVBLCtCQUVFLFdBQ0EsU0FDQTtBQUNBLFNBQU8sS0FBSyxLQUNWLHlCQUNBLFdBQVcsc0JBQXNCLDBDQUNqQyxDQUFDLFFBQVEsTUFBTSxPQUFPLFVBQVUsUUFBUSxNQUFNO0FBQUE7QUFJbEQsSUFBSSxVQUFVLElBQUksUUFBUSxhQUFhOyIsCiAgIm5hbWVzIjogW10KfQo=
+//# sourceMappingURL=data:application/json;base64,ewogICJ2ZXJzaW9uIjogMywKICAic291cmNlcyI6IFsiL2hvbWUvcnVubmVyL3dvcmsvbW9uZXkvbW9uZXkvc3JjL3l1cC1jb25maWcudHMiXSwKICAibWFwcGluZ3MiOiAiQUFBQTtBQUNBO0FBRUEsK0JBRUUsV0FDQSxTQUNBO0FBQ0EsU0FBTyxLQUFLLEtBQ1YseUJBQ0EsV0FBVyxzQkFBc0IsMENBQ2pDLENBQUMsUUFBUSxNQUFNLElBQUksUUFBUSxPQUFPLFFBQVE7QUFBQTtBQUk5QyxJQUFJLFVBQVUsSUFBSSxRQUFRLGFBQWE7IiwKICAibmFtZXMiOiBbXQp9Cg==
